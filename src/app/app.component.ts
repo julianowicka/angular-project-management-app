c55d046a65234c9c7aa5f5a6b0d12d67b30c284b
julia nowicka <julia.nowicka@lemondev.pl>
2024-03-21 23:01:05 +0100

    As part of the exercise a CSS class and an event were bind the component's dependencies were imported from the Standalone API and the NgFor directives were used for working with data collections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+export type Task = {
+  done: boolean;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [NgFor],
   template: `
-      <h1>
-        Welcome to {{title}}!
+      <h1 class="text-blue-500 bg-gray-200 py-4 text-xl text-center">
+        Another cool todolist :> 
       </h1>
+      <ul>
+        <li *ngFor="let task of tasks">
+          <button [class.line-through]="task.done" (click)="toggleDoneStatus(task)">
+            {{ task.name }}
+          </button>
+        </li>
+        </ul>
   `,
 })
 export class AppComponent {
   title = 'project-menagment-app';
+
+  tasks = [
+    {
+      name: "Task1",
+      done: false,
+    },
+    {
+      name: "Task2",
+      done: true,
+    },
+  ];
+  toggleDoneStatus(task: Task) {
+    task.done = !task.done
+
+  }
 }
